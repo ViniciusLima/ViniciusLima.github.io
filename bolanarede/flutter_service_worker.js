6487531aa2528005a920aa228c973c26f83fa5f0
Vinicius Lima <viniciuslima.fsa@gmail.com>
2020-02-19 14:02:23 -0300

    Ajustes telas home/login

--- bolanarede/flutter_service_worker.js
+++ bolanarede/flutter_service_worker.js
@@ -9,7 +9,7 @@ const RESOURCES = {
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "24f61b7fcf02d2868e0697c4f96c62a9",
-"/main.dart.js": "92484b89c807b3232015450708bea7ae",
+"/main.dart.js": "3dbc298bd9c23df3ff745287fb50227a",
 "/manifest.json": "2740554587450e9491c3a6f13d83b097"
 };
 
