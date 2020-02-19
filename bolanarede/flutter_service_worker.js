2c853f462a307260e80dbae0d6e87dfdbc086d56
Vinicius Lima <viniciuslima.fsa@gmail.com>
2020-02-19 12:18:32 -0300

    Atualizações

--- bolanarede/flutter_service_worker.js
+++ bolanarede/flutter_service_worker.js
@@ -2,14 +2,14 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"/assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"/assets/FontManifest.json": "ed9cd32fa597ae46abed4ac894b9902b",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/LICENSE": "2f2f057a6a33c9295cd6e2dcfdce98ef",
+"/assets/LICENSE": "72b33039ca0be2b1aae7b462fcf10d7f",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "24f61b7fcf02d2868e0697c4f96c62a9",
-"/main.dart.js": "6891410dd33832c44a95c31db30cb5ba",
+"/main.dart.js": "efdaeacecb3ba4cddf32241ca2a5af4a",
 "/manifest.json": "2740554587450e9491c3a6f13d83b097"
 };
 
