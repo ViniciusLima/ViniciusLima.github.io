1f3452420e5597fb50b61bc718326894dc0c8055
Vinicius Lima <viniciuslima.fsa@gmail.com>
2020-03-05 11:04:42 -0300

    Implementação de rodadas

--- bolanarede/flutter_service_worker.js
+++ bolanarede/flutter_service_worker.js
@@ -4,12 +4,12 @@ const RESOURCES = {
   "/assets\AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "/assets\FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
 "/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "4233bf1450bf5e80be85377b7b0cf88a",
+"/assets\LICENSE": "f4a8b9df8e61cb42aa75d8c1946deaa5",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "24f61b7fcf02d2868e0697c4f96c62a9",
-"/main.dart.js": "aac33194a48df5098a7bb6f789adccb7",
+"/main.dart.js": "e42a5229fef79d01eb27eeb0b02bfdeb",
 "/manifest.json": "2740554587450e9491c3a6f13d83b097"
 };
 
