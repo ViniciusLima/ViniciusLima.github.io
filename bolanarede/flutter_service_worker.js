30963c212d260ea5296a4c722cd63dcde2d635cc
Vinicius Lima <viniciuslima.fsa@gmail.com>
2020-04-24 13:26:49 -0300

    Alteração do ícone da login

--- bolanarede/flutter_service_worker.js
+++ bolanarede/flutter_service_worker.js
@@ -1,16 +1,17 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets\AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"/assets\FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
-"/assets\fonts\MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets\LICENSE": "863f86a2f5cf451d6fade527b497bad5",
-"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"/index.html": "c75b23920032e0a85e3d90d0312ef0f2",
-"/main.dart.js": "e4927cd449dff77176f8b3c61d803681",
-"/manifest.json": "2740554587450e9491c3a6f13d83b097"
+  "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"assets/FontManifest.json": "580ff1a5d08679ded8fcf5c6848cece7",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/LICENSE": "0882fbf438afe12c52748548050dbac7",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "c75b23920032e0a85e3d90d0312ef0f2",
+"/": "c75b23920032e0a85e3d90d0312ef0f2",
+"main.dart.js": "69ec499af117180aeaf2e3207600f6af",
+"manifest.json": "2740554587450e9491c3a6f13d83b097"
 };
 
 self.addEventListener('activate', function (event) {
@@ -32,9 +33,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-        return fetch(event.request, {
-          credentials: 'include'
-        });
+        return fetch(event.request);
       })
   );
 });
