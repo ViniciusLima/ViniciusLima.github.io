cec6538429d0f712a7745f19ea85c99cd56a8926
Vinicius Lima <viniciuslima.fsa@gmail.com>
2021-03-11 13:17:24 -0300

    Boladev v0.18

--- boladev/flutter_service_worker.js
+++ boladev/flutter_service_worker.js
@@ -3,43 +3,21 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "1b94a180e131813adbccc23b2cc5d11c",
-"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+  "assets/AssetManifest.json": "f5a066cfe613036d265687a3edfcf2f0",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/icon/gol.png": "99d76ab9eabf793ed298fdf9b3f90111",
 "assets/icon/logo.png": "cb422fdfca89fb613f3cc49d165a10bd",
-"assets/NOTICES": "604e80a8bf1273fc706b254f12875901",
-"favicon.png": "88a1e9d1176dfd96e85c7ed77bff3cb2",
-"icons/android-icon-144x144.png": "a886503822c0c6e524ab9b3e9f6e7245",
-"icons/android-icon-192x192.png": "06e7469a5d8edc4f567946f6888f3015",
-"icons/android-icon-36x36.png": "d0bab5363b8d19c9f04df8b49d5f8d37",
-"icons/android-icon-48x48.png": "b9c52a91fcc403e52b3b46acf58abc05",
-"icons/android-icon-72x72.png": "f6a3725ff8301bdbf982b45a6f88dae9",
-"icons/android-icon-96x96.png": "0bf1c4d8100ec68e1e25c0f06c8dad9b",
-"icons/apple-icon-114x114.png": "66dcca771f5ca5863d5bb2b3a7f749ff",
-"icons/apple-icon-120x120.png": "031f20818ba42a044eb2a7d9426c4415",
-"icons/apple-icon-144x144.png": "a886503822c0c6e524ab9b3e9f6e7245",
-"icons/apple-icon-152x152.png": "02384c0b67738dbd3bdaae3639004c8f",
-"icons/apple-icon-180x180.png": "0da0aa600a23897fc3a9e45743b22e1e",
-"icons/apple-icon-57x57.png": "e1f9a7fb04ceb2bdae6905347340b219",
-"icons/apple-icon-60x60.png": "e4bb17f1119827603201c7fee6d238cf",
-"icons/apple-icon-72x72.png": "f6a3725ff8301bdbf982b45a6f88dae9",
-"icons/apple-icon-76x76.png": "1f7769724ce0d52c3a7d89ffadd5c0e4",
-"icons/apple-icon-precomposed.png": "a1d08b70ea7a252c50fcaed30fdb64ec",
-"icons/apple-icon.png": "a1d08b70ea7a252c50fcaed30fdb64ec",
-"icons/favicon-16x16.png": "88a1e9d1176dfd96e85c7ed77bff3cb2",
-"icons/favicon-32x32.png": "3c3e317a2b212faa928a0a0a7571a10e",
-"icons/favicon-96x96.png": "0bf1c4d8100ec68e1e25c0f06c8dad9b",
-"icons/favicon.ico": "f3a968c87ad6889de397b9602674127d",
-"icons/ms-icon-144x144.png": "a886503822c0c6e524ab9b3e9f6e7245",
-"icons/ms-icon-150x150.png": "f04b88f9d0d3b8d88f9ab77567a27b07",
-"icons/ms-icon-310x310.png": "f3ba50c98773f882cfceb5e86299084f",
-"icons/ms-icon-70x70.png": "2e09ae99274ce40639c11ed65efba149",
-"index.html": "dcfda3e8aed5752d2781baf76510906f",
-"/": "dcfda3e8aed5752d2781baf76510906f",
-"main.dart.js": "9d7a7379f31da19c15b42ff7ac56735a",
-"manifest.json": "66f410c2c6b5d28a595953be34f8ef8b",
-"version.json": "567d89168d5f0c65cc99f3fc5feccc09"
+"assets/NOTICES": "25cae35513fc5fd1890bd696d77027ef",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "64d7315971cfb80c04244d0edd4d1492",
+"/": "64d7315971cfb80c04244d0edd4d1492",
+"main.dart.js": "29141cf535a9af7dc9a85db2fd4ecfcc",
+"manifest.json": "86830f5ac0ab6dab459e6be47468e4d4",
+"version.json": "f699187152e6888212ed870c1e76bcbc"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -183,7 +161,7 @@ async function downloadOffline() {
     }
     currentContent[key] = true;
   }
-  for (var resourceKey in Object.keys(RESOURCES)) {
+  for (var resourceKey of Object.keys(RESOURCES)) {
     if (!currentContent[resourceKey]) {
       resources.push(resourceKey);
     }
